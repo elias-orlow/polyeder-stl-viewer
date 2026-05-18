f61968e40cf56eb60faa79f676ad85df8d474325
Everstorm1 <ayenwohlberg@gmail.com>
2026-05-19 00:52:08 +0200

    3d Scene + Maus

---
+++ src/main/java/org/alegroup/polyederstlviewer/View/MainWindow/RenderingViewController.java
@@ -0,0 +1,104 @@
+package org.alegroup.polyederstlviewer.View.MainWindow;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Point3D;
+import javafx.scene.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Line;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
+
+public class RenderingViewController {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    private SubScene subScene;
+
+    // Für Mouse-Delta (FIX #3)
+    private double lastMouseX;
+    private double lastMouseY;
+
+    public void initialize() {
+
+        Group root3D = new Group();
+
+        // Licht
+        AmbientLight ambientLight = new AmbientLight(Color.rgb(80, 80, 80));
+        PointLight pointLight = new PointLight(Color.WHITE);
+        pointLight.setTranslateX(300);
+        pointLight.setTranslateY(300);
+        pointLight.setTranslateZ(300);
+        root3D.getChildren().addAll(ambientLight, pointLight);
+
+        // Camera
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setNearClip(0.1);
+        camera.setFarClip(10000.0);
+
+        // Wir schauen von unten nach oben hoch auf die Objekte
+        camera.setTranslateZ(-500);
+
+        // Test-Box mit Material (sonst: grauer Default)
+        Box box = new Box(20, 1, 1);
+        box.setTranslateX(20);
+        Box box2 = new Box(1, 20, 1);
+        box2.setTranslateY(20);
+        Box box3 = new Box(1, 1, 20);
+        box3.setTranslateZ(20);
+        PhongMaterial material = new PhongMaterial();
+        PhongMaterial material2 = new PhongMaterial();
+        PhongMaterial material3 = new PhongMaterial();
+        material.setDiffuseColor(Color.CORNFLOWERBLUE);
+        material2.setDiffuseColor(Color.RED);
+        material3.setDiffuseColor(Color.GREEN);
+        material.setSpecularColor(Color.WHITE);
+        material2.setSpecularColor(Color.WHITE);
+        material3.setSpecularColor(Color.WHITE);
+        box.setMaterial(material);
+        box2.setMaterial(material2);
+        box3.setMaterial(material3);
+        root3D.getChildren().addAll(box, box2, box3);
+
+        // SubScene
+        subScene = new SubScene(root3D, 100, 100, true, SceneAntialiasing.BALANCED);
+        subScene.setCamera(camera);
+
+        subScene.setFill(Color.rgb(40, 40, 40));
+
+        subScene.widthProperty().bind(rootPane.widthProperty());
+        subScene.heightProperty().bind(rootPane.heightProperty());
+
+        rootPane.getChildren().add(subScene);
+
+        // Rotation
+        Affine affine = new Affine();
+        root3D.getTransforms().add(affine);
+
+
+        subScene.setOnMousePressed(e -> {
+            lastMouseX = e.getSceneX();
+            lastMouseY = e.getSceneY();
+        });
+
+        subScene.setOnMouseDragged(e -> {
+            double dx = e.getSceneX() - lastMouseX;
+            double dy = e.getSceneY() - lastMouseY;
+
+            // (angle, pivotX, pivotY, pivotZ, axisX, axisY, axisZ)
+            affine.prependRotation( -dx * 0.3, 0, 0, 0,  0, 1, 0); // um Y-Weltachse
+            affine.prependRotation(dy * 0.3, 0, 0, 0,  1, 0, 0); // um X-Weltachse
+
+            lastMouseX = e.getSceneX();
+            lastMouseY = e.getSceneY();
+        });
+
+        //Zoom
+        subScene.setOnScroll(e -> {
+            camera.setTranslateZ(camera.getTranslateZ() + e.getDeltaY() * 0.5);
+        });
+    }
+}
