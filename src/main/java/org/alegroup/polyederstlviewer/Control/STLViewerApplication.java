bde4ae553fd492b4c46146e9f6ce61e6fa72df01
AW <ayenwohlberg@gmail.com>
2026-05-19 15:34:08 +0200

    main window size adapts to screen size

--- src/main/java/org/alegroup/polyederstlviewer/Control/STLViewerApplication.java
+++ src/main/java/org/alegroup/polyederstlviewer/Control/STLViewerApplication.java
@@ -3,6 +3,7 @@ package org.alegroup.polyederstlviewer.Control;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,10 @@ public class STLViewerApplication extends Application
     public void start (Stage stage) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(STLViewerApplication.class.getResource("/org/alegroup/polyederstlviewer/View/MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        // Scale Scene to fit screen (kind of)
+        double screenX = Screen.getPrimary().getBounds().getMaxX() - 150;
+        double screenY = Screen.getPrimary().getBounds().getMaxY() - 150;
+        Scene scene = new Scene(fxmlLoader.load(), screenX, screenY);
         stage.setTitle("STL-File Viewer!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/alegroup/polyederstlviewer/View/MainWindow/MainWindowController.java
+++ src/main/java/org/alegroup/polyederstlviewer/View/MainWindow/MainWindowController.java
@@ -1,6 +1,15 @@
 package org.alegroup.polyederstlviewer.View.MainWindow;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class MainWindowController {
 
+    @FXML
+    public BorderPane mainWindow;
 
+    public void initialize(){
+
+        //mainWindow.setScaleX(100);
+    }
 }
